678bc3827585e930f2e13cb1255145ad2629b682
Evgenii Grushin <evgenii.grushin@fortify.pro>
2025-03-30 23:24:44 -0700

    причесал структуру папок

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Navbar from '../../components/Navbar';
+import Navbar from '@/components/Navbar';
 
 export default function DashboardPage() {
   const router = useRouter();
